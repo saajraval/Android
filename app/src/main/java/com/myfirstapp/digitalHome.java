8567dbb12a8f285e002521a9993080ce02b08545
saajraval <saajravals7r@gmail.com>
2022-06-02 12:47:23 +0530

    insta and didital home layout (relative)

---
+++ app/src/main/java/com/myfirstapp/digitalHome.java
@@ -0,0 +1,14 @@
+package com.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class digitalHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_digital_home);
+    }
+}
