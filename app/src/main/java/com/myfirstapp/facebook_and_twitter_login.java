8dc1234fb24eb4dce46dba0d6b4af50b9f5b31b9
saajraval <saajravals7r@gmail.com>
2022-06-06 19:18:08 +0530

    lenear layout for facbook and twitter

---
+++ app/src/main/java/com/myfirstapp/facebook_and_twitter_login.java
@@ -0,0 +1,14 @@
+package com.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class facebook_and_twitter_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook_and_twitter_login);
+    }
+}
